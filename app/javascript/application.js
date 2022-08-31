74f9788a57948dab2f854a20e91acc6a2f3307b2
Yors <jatbribiesca@gmail.com>
2022-08-31 18:43:44 -0500

    add log in and log out functionality and style

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,10 @@ import "controllers"
 $(document).on('turbo:load', () => {
   $('.ui.dropdown').dropdown();
 });
+
+$('.message .close').on('click', function() {
+  $(this)
+    .closest('.message')
+    .transition('fade')
+  ;
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
